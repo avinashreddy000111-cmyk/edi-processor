bbbeb3d8697cff4f42c398595755bd2f660b74cc
avinashreddy000111-cmyk <avinashreddy000111@gmail.com>
2026-01-19 01:27:30 +0530

    Update EdiProcessorService.java
    fix

--- src/main/java/com/edi/processor/service/EdiProcessorService.java
+++ src/main/java/com/edi/processor/service/EdiProcessorService.java
@@ -239,7 +239,7 @@ public class EdiProcessorService {
 
         // GETSCHEMA Transaction - always returns 1 response
         if (TRANSACTION_TYPE_GETSCHEMA.equalsIgnoreCase(transactionType)) {
-            return handleGetSchemaTransaction(format, responseType, uuid);
+            return handleGetSchemaTransaction(transactionType,format, responseType, uuid);
         }
 
         // ORDER Transaction
@@ -265,11 +265,14 @@ public class EdiProcessorService {
      * Handle GETSCHEMA Transaction - Returns 1 response
      * Valid Response Types: ASN, ITEM, SHIPCONFIRM, RECEIPT
      */
-    private EdiResponse handleGetSchemaTransaction(String format, String responseType, String uuid) {
+    private EdiResponse handleGetSchemaTransaction(String transactionType, String format, String responseType, String uuid) {
         String mimeType = determineMimeType(format);
         String fileExtension = determineFileExtension(format);
-
-        String filename = "GETSCHEMA_" + responseType + "_" + uuid + "." + fileExtension;
+        if (orderType != null) {
+                String filename = transactionType + "_" + responseType + "_" + orderType + "_" + uuid + "." + fileExtension;
+            } else {
+                String filename = transactionType + "_" + responseType + "_" + uuid + "." + fileExtension;
+            }
         String content = contentProvider.getGetSchemaContent(responseType);
 
         return buildSuccessResponse(filename, content, mimeType);
